7f1a904029e641e237dca4f3d6a69af5e1594103
Santosh Nijalingappa <santosh.nijalingappa@cognizant.com>
2020-04-29 22:11:21 +0530

    initial commit

---
+++ com/bigdata/app/src/main/java/SparkDataSetOperationsForCsv.java
@@ -0,0 +1,20 @@
+package main.java;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class SparkDataSetOperationsForCsv {
+
+    public static void main(String[] args){
+        //Use SparkSession to create Dataset<Row>
+        SparkSession spark = SparkSession.builder()
+                .master("local")
+                .appName("Apple Stock Operations")
+                .config("spark.some.config.option", "some-value")
+                .getOrCreate();
+
+        Dataset<Row>  appleStockDataSet = spark.read().format("csv").option("header",true).option("inferSchema",true).load("D:\\SparkBasicsJavaAndScala\\com\\bigdata\\app\\src\\main\\resources\\appl_stock.csv");
+        appleStockDataSet.show();
+    }
+}
---
+++ com/bigdata/app/src/main/scala/SparkScalaDataSetOperationsForCsv.scala
@@ -0,0 +1,19 @@
+package main.scala
+
+import org.apache.spark.sql.SparkSession
+
+object SparkScalaDataSetOperationsForCsv {
+
+  def main(args: Array[String]): Unit ={
+    //Use SparkSession to create Dataset<Row> which is DataFrame
+    val spark = SparkSession.builder.master("local").appName("Apple Stock Operations").config("spark.some.config.option", "some-value").getOrCreate;
+    val applDataSet = spark.read.format("csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load("D:\\SparkBasicsJavaAndScala\\com\\bigdata\\app\\src\\main\\resources\\appl_stock.csv")
+
+    applDataSet.show()
+    applDataSet.printSchema()
+  }
+
+}
